79eed7296cb279402cfc435082313f0654092062
pushpaja63 <pushpaja.allamsetty@gmail.com>
2025-12-10 15:40:02 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-base: './Cancer-support-webapp/',
+base: "./Cancer-support-webapp/",
   plugins: [react(),tailwindcss(),],
 })
