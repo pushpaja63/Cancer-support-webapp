e75834b9726f246fd857dec828e1af86bbd8d2fa
pushpaja63 <pushpaja.allamsetty@gmail.com>
2025-12-10 13:04:27 +0530

    commit changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./ index.html";
+
 
 createRoot(document.getElementById("root")).render(<App />);
